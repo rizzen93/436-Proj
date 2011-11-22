419b7ea452afb161a9574af2af2757fe1a1b70af
rizzen93 <rizzen93@gmail.com>
2011-11-21 22:38:34 -0600

    Initial commit for base classes... again.

---
+++ ircapp/src/model/Message.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.Date;
+
+/**
+ * Default class to hold a message from server/user/whatever
+ * @author ryan
+ *
+ */
+public class Message 
+{
+
+	private final String sender;
+	private final String text;
+	private final long timestamp;
+	
+	public Message(String sender, String text)
+	{
+		this.text = text;
+		this.sender = sender;
+		this.timestamp = new Date().getTime();
+	}
+	
+	public String getSender()
+	{
+		return this.sender;
+	}
+	
+	public String getText()
+	{
+		return this.text;
+	}
+	
+	public long getTimestamp()
+	{
+		return this.timestamp;
+	}
+	
+	
+}
