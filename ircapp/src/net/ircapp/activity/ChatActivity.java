503bd554bf48c56f47dca27e01d5ee21063a8412
rizzen93 <rizzen93@gmail.com>
2011-12-04 01:29:26 -0600

    Let's try this again, without the random god damn NullPointerExceptions that seem to bloody plague me.

--- ircapp/src/net/ircapp/activity/ChatActivity.java
+++ ircapp/src/net/ircapp/activity/ChatActivity.java
@@ -1,15 +1,63 @@
 package net.ircapp.activity;
 
+import net.ircapp.IRCApp;
 import net.ircapp.R;
+import net.ircapp.commands.Constants;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ChatActivity extends Activity
 {
 	
+	private EditText chatLog;
+	private EditText chatLine;
+	private Button sendButton;
+	
+	/**
+	 * On Create
+	 */
 	 public void onCreate(Bundle savedInstanceState)
 	 {
 		 super.onCreate(savedInstanceState);
 		 setContentView(R.layout.channelview);
+		 
+		 Bundle extras = this.getIntent().getExtras();
+		 
+		 if(extras != null)
+			 System.out.println("WE GOT EXTRAS HERE");
+		 
+		 this.chatLog = (EditText) findViewById(R.id.channelview_chatLog);
+		 this.chatLine = (EditText) findViewById(R.id.channelview_chatLine);
+		 this.sendButton = (Button) findViewById(R.id.channelview_sendButton);
+		 
+		 
+		 this.sendButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) 
+			{
+				String text = chatLine.getText().toString().trim();
+				System.out.println("Got text: " + text);
+				
+				chatLine.setText("");
+				chatLog.append(text);
+				
+				sendText(text);
+			}
+		 });
+	 }
+	 
+	 /**
+	  * Sends the specified text to the server
+	  * @param text
+	  */
+	 public void sendText(String text)
+	 {
+		 //IRCApp.getInstance().sendText(servID, Constants.PRIVMSG, channelName, text);
 	 }
 }
+
